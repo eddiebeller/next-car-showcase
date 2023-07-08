da21abc59a3ca515b7700f186e6f074d0b6f7c6b
Eduard Beller <ieduard.beller@gmail.com>
2023-07-08 18:49:58 +0200

    Add footer and stubdata

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import { footerLinks } from '@/constants';
+import Link from 'next/link';
 
 export const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<footer>
+			<div className='flex flex-col text-black-100 mt-5 border-t border-gray-100'>
+				<div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+					<div className='flex flex-col justify-start items-start gap-6'>
+						<Image
+							src='./logo.svg'
+							alt='logo'
+							width={118}
+							height={18}
+							className='object-contain'
+						/>
+						<p className='text-base text-gray-700'>
+							CarHub 2023 <br />
+							All rights reserved &copy;
+						</p>
+					</div>
+
+					<div className='footer__links'>
+						{footerLinks.map((link, i) => (
+							<div key={i} className='footer__link'>
+								<h3 className='font-bold'>{link.title}</h3>
+								{link.links.map((link, i) => (
+									<Link href={link.url} key={i} className='text-gray-500'>
+										{link.title}
+									</Link>
+								))}
+							</div>
+						))}
+					</div>
+					<div className='flex justify-between items-center flex-wrap mt10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+						<p>@Curhub 2023. All Right Reserved.</p>
+						<div className='footer__copyrights-link'>
+							<Link href='/' className='text-gray-500'>
+								Privacy Policy
+							</Link>
+							<Link href='/' className='text-gray-500'>
+								Term of use
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
+};
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -6,11 +6,11 @@ import { Button } from './Button';
 export const Navigation = () => {
 	return (
 		<header className='w-full absolute z-10'>
-			<nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:p-16 px-6 py-4'>
+			<nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4'>
 				<Link href='/' className='flex items-center justify-center'>
 					<Image
 						src='./logo.svg'
-						alt='car showcase logo'
+						alt='Logo'
 						width={180}
 						height={18}
 						className='object-contain'
@@ -19,7 +19,7 @@ export const Navigation = () => {
 				<Button
 					title='Sign In'
 					containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
-          buttonType='button'
+					buttonType='button'
 				/>
 			</nav>
 		</header>
