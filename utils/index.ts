f32e06103ec724f57c31721cdb515c521a66792d
Eduard Beller <ieduard.beller@gmail.com>
2023-07-10 19:26:15 +0200

    :bug: hide apikey

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,11 @@
 export async function fetchAllCars() {
+	console.log(process.env.API_KEY);
 	const headers = {
-		'X-RapidAPI-Key': '901dac17afmsh1440451628a2d97p187042jsn5476e002e371',
+		'X-RapidAPI-Key': process.env.API_KEY,
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
 	};
 	const response = await fetch(
-		`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=civic`,
+		`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=mercedes`,
 		{
 			headers: headers,
 		}
